11074d51050f4cb02f3889f7d3045d581ffb9852
danialmkj <www.danimalekdev@gmail.com>
2025-02-16 13:18:35 +0330

    feat: add EditDetailScreen & complete Navigation

--- app/src/main/java/com/example/wishlistapp/HomeView.kt
+++ app/src/main/java/com/example/wishlistapp/HomeView.kt
@@ -19,11 +19,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.wishlistapp.data.DummyWish
 import com.example.wishlistapp.data.Wish
 
 @Composable
-fun HomeView() {
+fun HomeView(
+    navController: NavController,
+    viewModel: WishViewModel
+) {
     Scaffold(
         topBar = { AppBarView(title = "WishList ", onBackNavClicked = {}) },
         floatingActionButton = {
@@ -33,6 +37,7 @@ fun HomeView() {
                 containerColor = Color.Black,
                 onClick = {
                     //TODO Add Navigation to Add Screen
+                    navController.navigate(Screen.AddScreen.routeName)
                 },
             ) { Icon(imageVector = Icons.Default.Add, contentDescription = null) }
         }) {
--- app/src/main/java/com/example/wishlistapp/WishViewModel.kt
+++ app/src/main/java/com/example/wishlistapp/WishViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.wishlistapp
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class WishViewModel : ViewModel() {
+
+    //we want to keep data, get data and load our data here
+    var wishTitleState by mutableStateOf("")
+    var wishDescriptionState by mutableStateOf("")
+
+
+    fun wishTitleChange(newString: String) {
+        wishTitleState = newString
+    }
+
+    fun wishDescriptionChange(newString: String) {
+        wishDescriptionState = newString
+    }
+
 }
