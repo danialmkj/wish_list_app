8cfbf21f93f2aff1a77aa02239490cfb7ccdf553
danialmkj <www.danimalekdev@gmail.com>
2025-02-26 11:06:40 +0330

    feat: initialize Database in Graph object

--- app/src/main/java/com/example/wishlistapp/Graph.kt
+++ app/src/main/java/com/example/wishlistapp/Graph.kt
@@ -5,10 +5,15 @@ import androidx.room.Room
 import com.example.wishlistapp.data.WishDatabase
 import com.example.wishlistapp.data.WishRepository
 
+// object Graph is simple example of dependency injection in kotlin
+//object graph declares a singleton named graph
+//object keyword is used to declare a singleton
+//A Singleton is a class of which only one instance will exist in the application which means that we can't create another graph object
+//so this object grapph use as a service locator
 object Graph {
 
     //we want to initialize DataBase
-
+    // lateinit is a non-nullable property database
     lateinit var database: WishDatabase
 
     // Initialize wishRepository when first needed.
